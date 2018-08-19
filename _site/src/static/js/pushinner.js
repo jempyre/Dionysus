f4d14596d0904993a541a86c58d76a1af1e00bbd
Jonah E Havel <jonah.havel@protonmail.ch>
2018-08-19 09:51:40 -0500

    nmb

---
+++ _site/src/static/js/pushinner.js
@@ -0,0 +1,15 @@
+/*
+ * set an element pair that will be pushing each other  */
+function setElmPair(pusherid, pushinid){
+    pusher = document.getElementById(pusherid)
+    pushin = document.getElementById(pushinid)
+
+    /* build this function object and give it to the caller to assign to an element.
+    each time called, it toggles and recycles thre default value*/
+    return function(pushin_dept="-250px") {
+        pushin_dept = -1*pushin_dept
+        pusher.style.width += pushin_dept;
+        pushin.style.marginLeft -= pushin_dept;
+        return pushin_dept
+    }
+}
