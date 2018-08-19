efad251c14e3942cdecd59fbd8e5951612bb51f5
Jonah E Havel <jonah.havel@protonmail.ch>
2018-08-19 08:16:20 -0500

    updates

---
+++ src/static/js/sidenav.js
@@ -0,0 +1,20 @@
+/* Set the width of the side navigation to `wtoc` and the left margin of the page content to wmain */
+function setElmPair(tocid, mainid){
+    sidenav = document.getElementById(tocid)
+    main = document.getElementById(mainid)
+
+function openNav(wtoc, wmain, target, margin) {
+    target.style.width = wtoc;
+    .style.marginLeft = wmain;
+}
+
+/* Set the width of the side navigation to wtoc and the margin of main to wmain */
+function closeNav(wtoc, wmain, tocid) {
+    document.getElementById(tocid).style.width = wtoc;
+    document.getElementById("main").style.marginLeft = wmain;
+}
+<link id=#pusher20x236ft7u42>
+datalist.pusher{
+    --k0x67vL: setElmPair(sidenav, main)
+</link>
+\u0042 - 0x20 = \u0023  0
